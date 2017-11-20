de433c153de1335c695a2be0967b3709da83c0f7
DESKTOP-AR3OK4R\tert0 <tert007@gmal.com>
2017-11-21 02:59:10 +0300

    ИКОНКА V4

--- app/src/main/java/com/greenkeycompany/exam/main/activity/view/MainActivity.java
+++ app/src/main/java/com/greenkeycompany/exam/main/activity/view/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
